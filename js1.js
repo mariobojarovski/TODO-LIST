57b9680125717f18eacc50614c02b51a9657c234
Mario Bojarovski <mb28484@seeu.edu.mk>
2019-11-14 17:23:41 +0100

    Mario

--- js1.js
+++ js1.js
@@ -12,24 +12,39 @@ function inputLength() {
 }
 
 function createListElement() {
+    
     var li = document.createElement("li"); // creates an element "li"
     li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
     ul.appendChild(li); //adds li to ul
     input.value = ""; //Reset text input field
-
+    
     var delBtn = document.createElement("button");
     delBtn.appendChild(document.createTextNode("X"));
     li.appendChild(delBtn);
     delBtn.addEventListener("click", () => deleteListItem(li));
+
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "checkbox");
+    li.appendChild(x);
+
+    
     // delBtn.index = i;
     // console.log(delBtn.index);
     // i++;
     // myArr.push(i);
+    x.addEventListener("click", () => check(li));//click list
+
+    function check(element){
+        console.log(2)
+        element.parentNode.classList.add("delStyle");
+        
+    }
 
     function deleteListItem(element) {
         // console.log("before pop", i, myArr);
         // myArr.pop(i);
         // console.log("after pop", i, myArr);
+        console.log("X")
         element.parentNode.removeChild(element);
     }
 }
@@ -40,13 +55,14 @@ function addListAfterClick() {
     }
 }
 
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
+function addTaskOnKeyPress(event){
+    if (event.keyCode === 13 && inputLength() > 0) {
         event.preventDefault();
         createListElement();
         //document.getElementById("delBtn").onclick;
         console.log(event.keyCode);
     }
-});
+}
 
 enterButton.addEventListener("click", addListAfterClick);
+input.addEventListener("keyup", addTaskOnKeyPress);
