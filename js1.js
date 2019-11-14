a0d4093600a43c54f37e1996af185f8a9e942ca8
Mario <bojarovskim@gmail.com>
2019-11-14 09:58:26 +0100

    Mario

--- js1.js
+++ js1.js
@@ -8,41 +8,45 @@ var item = document.getElementsByTagName("li");
 // let i = 0;
 
 function inputLength() {
-  return input.value.length;
+    return input.value.length;
 }
 
 function createListElement() {
-  var li = document.createElement("li"); // creates an element "li"
- li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-  ul.appendChild(li); //adds li to ul
-  input.value = ""; //Reset text input field
+    var li = document.createElement("li"); // creates an element "li"
+    li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+    ul.appendChild(li); //adds li to ul
+    input.value = ""; //Reset text input field
 
-  var delBtn = document.createElement("button");
-  delBtn.appendChild(document.createTextNode("X"));
-  li.appendChild(delBtn);
-  delBtn.addEventListener("click", () => deleteListItem(li));
-  // delBtn.index = i;
-  // console.log(delBtn.index);
-  // i++;
-  // myArr.push(i);
+    var delBtn = document.createElement("button");
+    delBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(delBtn);
+    delBtn.addEventListener("click", () => deleteListItem(li));
+    // delBtn.index = i;
+    // console.log(delBtn.index);
+    // i++;
+    // myArr.push(i);
 
-  function deleteListItem(element) {
-    // console.log("before pop", i, myArr);
-    // myArr.pop(i);
-    // console.log("after pop", i, myArr);
-    element.parentNode.removeChild(element);
-  }
+    function deleteListItem(element) {
+        // console.log("before pop", i, myArr);
+        // myArr.pop(i);
+        // console.log("after pop", i, myArr);
+        element.parentNode.removeChild(element);
+    }
 }
+
 function addListAfterClick() {
-  if (inputLength() > 0) {
-    createListElement();
-  }
+    if (inputLength() > 0) {
+        createListElement();
+    }
 }
-function check(event) {
-  if (event.which === 13) {
-   event.createListElement();
-  }
-}
+
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        createListElement();
+        //document.getElementById("delBtn").onclick;
+        console.log(event.keyCode);
+    }
+});
 
 enterButton.addEventListener("click", addListAfterClick);
-enterButton.addEventListener("keyup", check(event));
